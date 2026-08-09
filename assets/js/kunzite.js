8c66e18dd1264946a0979da80b24856335a377fd
Dityay <radityaikadek850@gmail.com>
2026-08-09 20:01:55 +0800

    ColorOS placeholder

--- assets/js/kunzite.js
+++ assets/js/kunzite.js
@@ -1,28 +1,10 @@
 window.kunziteData = [
     {
-        id: "kun_myui8",
-        name: "???",
+        id: "kun_cos16",
+        name: "ColorOS 16",
         category: "ROM",
         device: "Xiaomi Redmi Note 15 5G (kunzite)",
-        version: "???",
-        buildDate: "25-12-25",
-        downloadUrl: "",
-        banner: "",
-        screenshots: [
-            ""
-        ],
-
-        description: "",
-        notes: "",
-        flashInstruction: "",
-        credits: ""
-    },
-    {
-        id: "kun_orfox",
-        name: "OrangeFox",
-        category: "Others",
-        device: "Xiaomi Redmi Note 15 5G (kunzite)",
-        version: "???",
+        version: "Android 16 (BETA)",
         buildDate: "25-12-25",
         downloadUrl: "",
         banner: "",

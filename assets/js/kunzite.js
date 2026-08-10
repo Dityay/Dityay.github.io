202e1cb41f3643de1318d9348ed9d42f5b805b48
Dityay <radityaikadek850@gmail.com>
2026-08-10 22:53:32 +0800

    COS

--- assets/js/kunzite.js
+++ assets/js/kunzite.js
@@ -5,16 +5,15 @@ window.kunziteData = [
         category: "ROM",
         device: "Xiaomi Redmi Note 15 5G (kunzite)",
         version: "Android 16 (BETA)",
-        buildDate: "25-12-25",
-        downloadUrl: "",
-        banner: "",
+        buildDate: "2026-12-25",
+        downloadUrl: "a",
+        banner: "assets/roms/kunzite/cos16/banner.png",
         screenshots: [
             ""
         ],
 
-        description: "",
-        notes: "",
-        flashInstruction: "",
-        credits: ""
+        description: "# \"Here I come San Frandisco!\"\n### Notes\n- **THIS IS A BETA BUILD, YOUR FEEDBACK IS REALLY NEEDED.**\n- Let the ROM settle for a while after booting\n- Non rooted by default and shipped with OrangeFox\n### Known Issues\n- FOD and FaceUnlock not working\n- Delayed call mic (myb)\n- Report to me if you found any other issues",
+        flashInstruction: "### Installation\n- Flash [OrangeFox](https://t.me/KunziteIDUpdates/38)\n- Flash the ROM file  (recommended flashing either from Internal storage or adb sideload)\n- Format data\n- Reboot if you dont want to flash any other things",
+        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps and instructions\n- @ArticUpdates, @eepyvanagrand for settings mod\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
     }
 ];

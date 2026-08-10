175adfb6f910dd0ea11aa4f6b514b0b8b4a413a7
Dityay <radityaikadek850@gmail.com>
2026-08-10 23:14:36 +0800

    vendor

--- assets/js/kunzite.js
+++ assets/js/kunzite.js
@@ -14,6 +14,6 @@ window.kunziteData = [
 
         description: "# \"Here I come San Frandisco!\"\n### Notes\n- **THIS IS A BETA BUILD, YOUR FEEDBACK IS REALLY NEEDED.**\n- Let the ROM settle for a while after booting\n- Non rooted by default and shipped with OrangeFox\n### Known Issues\n- FOD and FaceUnlock not working\n- Delayed call mic (myb)\n- Report to me if you found any other issues",
         flashInstruction: "### Installation\n- Flash [OrangeFox](https://t.me/KunziteIDUpdates/38)\n- Flash the ROM file  (recommended flashing either from Internal storage or adb sideload)\n- Format data\n- Reboot if you dont want to flash any other things",
-        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps and instructions\n- @ArticUpdates, @eepyvanagrand for settings mod\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
+        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps and instructions\n- @ArticUpdates, @eepyvanagrand for settings mod\n- @heybyben for aosp vendor\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
     }
 ];

2c93f2f135bef11aedac59036ae0f4a2c8164684
Dityay <radityaikadek850@gmail.com>
2026-08-12 18:27:18 +0800

    cos

--- assets/js/kunzite.js
+++ assets/js/kunzite.js
@@ -9,11 +9,16 @@ window.kunziteData = [
         downloadUrl: "a",
         banner: "assets/roms/kunzite/cos16/banner.png",
         screenshots: [
-            ""
+            "assets/roms/kunzite/cos16/1.jpg",
+            "assets/roms/kunzite/cos16/2.jpg",
+            "assets/roms/kunzite/cos16/3.jpg",
+            "assets/roms/kunzite/cos16/4.jpg",
+            "assets/roms/kunzite/cos16/5.jpg",
+            "assets/roms/kunzite/cos16/6.jpg"
         ],
 
-        description: "# \"Thank you for watching my dream...\"\n### Notes\n- **THIS IS A BETA BUILD, YOUR FEEDBACK IS REALLY NEEDED.**\n- Let the ROM settle for a while after booting\n- Non rooted by default and shipped with OrangeFox\n### Known Issues\n- FOD and FaceUnlock not working\n- Delayed call mic (myb)\n- Report to me if you found any other issues",
+        description: "# \"Thank you for watching my dream...\"\n### Notes\n- **THIS IS A BETA BUILD, YOUR FEEDBACK IS REALLY NEEDED.**\n- Let the ROM settle for a while after booting (may lag after booting, especially for the first boot)\n- Non rooted by default and shipped with OrangeFox\n### Known Issues\n- FOD and FaceUnlock not working\n- Delayed call mic (myb)\n- Report to me if you found any other issues",
         flashInstruction: "### Installation\n- Flash [OrangeFox](https://t.me/KunziteIDUpdates/38)\n- Flash the ROM file  (recommended flashing either from Internal storage or adb sideload)\n- Format data\n- Reboot if you dont want to flash any other things",
-        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps and instructions\n- @ArticUpdates, @eepyvanagrand for settings mod\n- [Danda](https://github.com/Danda420) for femboy :eyes: AIDL (cryptoeng)\n- @heybyben for aosp vendor\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
+        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps and instructions\n- @ArticUpdates, @eepyvanagrand for settings mod\n- @heybyben for aosp vendor\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
     }
 ];

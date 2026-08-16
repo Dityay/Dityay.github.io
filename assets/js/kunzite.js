d07d129bf7126b8696b2b9d78abae29886fe271d
Dityay/ <radityaikadek850@gmail.com>
2026-08-16 21:35:03 +0800

    Update

--- assets/js/kunzite.js
+++ assets/js/kunzite.js
@@ -5,7 +5,7 @@ window.kunziteData = [
         category: "ROM",
         device: "Xiaomi Redmi Note 15 5G (kunzite)",
         version: "Android 16 (BETA)",
-        buildDate: "2026-12-25",
+        buildDate: "2026-8-17",
         downloadUrl: "a",
         banner: "assets/roms/kunzite/cos16/banner.png",
         screenshots: [
@@ -17,8 +17,8 @@ window.kunziteData = [
             "assets/roms/kunzite/cos16/6.jpg"
         ],
 
-        description: "# \"Thank you for watching my dream...\"\n### Notes\n- **THIS IS A BETA BUILD, YOUR FEEDBACK IS REALLY NEEDED.**\n- Let the ROM settle for a while after booting (may lag after booting, especially for the first boot)\n- Non rooted by default and shipped with OrangeFox\n### Known Issues\n- FOD and FaceUnlock not working\n- Delayed call mic (myb)\n- Report to me if you found any other issues",
-        flashInstruction: "### Installation\n- Flash [OrangeFox](https://t.me/KunziteIDUpdates/38)\n- Flash the ROM file  (recommended flashing either from Internal storage or adb sideload)\n- Format data\n- Reboot if you dont want to flash any other things",
-        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps and instructions\n- @ArticUpdates, @eepyvanagrand for settings mod\n- @heybyben for aosp vendor\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
+        description: "# \"Skies have been forever blue...\"\n### Notes\n- **THIS IS A BETA BUILD, YOUR FEEDBACK IS REALLY NEEDED.**\n- Let the ROM settle for a while after booting (may lag after booting, especially for the first boot)\n- Non rooted by default and shipped with OrangeFox\n- **Thermal is disabled by default, to enable run ```setprop persist.deltacore.disable_thermal false``` in terminal with root**\n### Known Issues\n- FOD and FaceUnlock not working\n- Light sensor (auto brightness) not working\n- Report to me if you found any other issues",
+        flashInstruction: "### Installation\n- Flash [this](https://t.me/KunziteIDUpdates/38) or [this](https://t.me/archivebsct2/35) recovery\n- Flash the ROM file  (recommended flashing either from Internal storage or adb sideload)\n- Format data\n- Reboot if you dont want to flash any other things",
+        credits: "- @KanagawaYamadaVTeacher, @eepyvanagrand for alot of helps, instructions and fixes\n- @ArticUpdates, @eepyvanagrand for settings mod\n- @heybyben for aosp vendor\n- [@EryneCad](https://x.com/EryneCad/status/2080014147471978672) for kriscest art :eyes:"
     }
 ];
